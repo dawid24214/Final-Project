08760d9e4a5eb51b9fb0c3f6ce1bdae3ae1b2535
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-02 21:42:33 +0200

    Sure! Here's the translation:  "Completion of Environment Configuration"

---
+++ vite-project/src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function Header() {
+    return (
+        <div>
+            <h1>Jest  Tracker</h1>
+            <p>Keep track of your expenses</p>
+        </div>
+    );
+}
+
+export default Header;
---
+++ vite-project/src/Header/Test1.jsx
@@ -0,0 +1,9 @@
+function Test1() {
+  return (
+    <div>
+      <h1>React Groszek</h1>
+      <p>React is a JavaScript library for building user interfaces.</p>
+    </div>
+  );
+}
+export default Test1;
