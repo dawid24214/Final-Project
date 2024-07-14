a1f2419046d1b4ada78c54474490bae31bad847b
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-14 11:21:06 +0200

    App.jsx/
    Przygotowanie Importu plików ,

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,27 @@
-import Header from './component/Header/Header.jsx';
+import "./App.css"
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {Navbar} from "./components/navbar.jsx"
+import {Shop} from "./pages/shop/shop.jsx"
+import {Contact} from "./contact.jsx"
+import {Cart} from "./pages/cart/cart.jsx"
+import {ShopContextProvider} from './context/shop-context.jsx'
 
-function App() {
-    return (
-        <div>
-            <Header />
+
+function App () {
+    return(
+        <div className="App">
+            <ShopContextProvider>
+                <Router>
+                    <Navbar/>
+                        <Routes>
+                            <Route></Route>
+                            <Route></Route>
+                            <Route></Route>
+                        </Routes>
+                </Router>
+            </ShopContextProvider>
         </div>
-    );
+    )
 }
 
 export default App;
