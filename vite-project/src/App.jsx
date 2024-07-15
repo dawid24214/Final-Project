b635e936f9d42a41c049daca253a196046bd8958
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-15 13:00:52 +0200

    Modyfkacja karty produktu

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,29 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
-/*import Shop from "./pages/shop/shop.jsx";
+import Shop from "./pages/shop/shop.jsx";
 import Contact from "./contact.jsx";
-import Cart from "./pages/cart/cart.jsx";*/
-/*
+import Cart from "./pages/cart/cart.jsx";
+
 import ShopContextProvider from './context/shop-context.jsx'
-</ShopContextProvider>
- <ShopContextProvider>
-*/
+
+
 
 
 function App () {
     return(
-        <div className="App"><Router>
+        <div className="App">
+            <ShopContextProvider>
+            <Router>
             <Navbar/>
             <Routes>
                 <Route path='/' />
                 <Route path='/contact'  />
                 <Route path='/cart' />
             </Routes>
-        </Router></div>
+        </Router>
+        </ShopContextProvider>
+        </div>
     );
 }
 
--- vite-project/src/pages/cart/cart.jsx
+++ vite-project/src/pages/cart/cart.jsx
@@ -1,1 +1,24 @@
-default
+import React, {useContext} from "react";
+import PRODUCTS  from "../../products.js";
+import ShoppContext from 'src/context/shop-context.jsx';
+import CartItem from 'src/pages/cart/cart-item.jsx';
+
+import {useNavigation} from "react-router-dom";
+
+import './_cart.scss';
+import products from "../../products.js";
+
+const  Cart = () => {
+    const {cartItems, getTotalCartAmount, checkout} = useContext(ShoppContext);
+    return(
+        <div className="cart">
+            {PRODUCTS.map((product)=>{
+                if (cartItems[product.id] !==0) {
+                    return <CartItem data={product} />;
+                }
+            } )}
+
+
+        </div>
+    )
+}
