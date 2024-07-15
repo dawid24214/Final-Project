7db0e5b3bfea5903fb8b8828c4798844d0e6fb3d
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-15 12:13:05 +0200

    Produkty
    Dodawanie - nazwy ,ceny ,zdjęć produktów .

---
+++ vite-project/src/products.js
@@ -0,0 +1,40 @@
+import product1 from './assets/Zestaw-plastyczny-do-malowania-w-walizce-145el-rozowy-104756(2).jpg';
+import product2 from './assets/Zestaw-plastyczny-do-malowania-w-walizce-208-elementow-kredki-pastele-markery-pisaki-farby-linijka-niebieski-141168.jpg';
+import product3 from './assets/Karty-do-gry-pokera-plastikowe-zlote-w-ozdobnej-szkatulce-140010.jpg';
+import product4 from './assets/Grill-ogrodowy-weglowy-na-brykiet-zamykany-z-pokrywa-wentylacja-i-polka-149825.jpg';
+import product5 from './assets/Dron-F9-kamera-6K-HD-GPS-WIFI-zasieg-2000m-103374(2).jpg';
+
+const PRODUCTS = [
+    {
+        id:1,
+        productName:'Zestaw plastyczny ',
+        price:62.0,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName:'Kretki ,pastele, pisaki',
+        price:48.0,
+        productImage:product2,
+    },
+    {
+        id:3,
+        productName:"Karty do gry w pokera",
+        price:55.0,
+        productImage:product3,
+    },
+    {
+        id:4,
+        productName:'Grill ogrodowy ,weglowy',
+        price:124.0,
+        productImage:product4,
+    },
+    {
+        id:5,
+        productName:"Dron-9K",
+        price:1257.0,
+        productImage:product5,
+    },
+
+];
+export default PRODUCTS;
