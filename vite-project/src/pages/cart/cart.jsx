2a930603dd8dae1fe00c172ca152b04e3ecfc65b
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-17 09:22:55 +0200

    cart

--- vite-project/src/pages/cart/cart.jsx
+++ vite-project/src/pages/cart/cart.jsx
@@ -2,23 +2,49 @@ import React, {useContext} from "react";
 import PRODUCTS  from "../../products.js";
 import ShoppContext from 'src/context/shop-context.jsx';
 import CartItem from 'src/pages/cart/cart-item.jsx';
+import {useNavigate} from "react-router-dom";
 
-import {useNavigation} from "react-router-dom";
+
 
 import './_cart.scss';
 import products from "../../products.js";
 
 const  Cart = () => {
     const {cartItems, getTotalCartAmount, checkout} = useContext(ShoppContext);
+    const totalAmount = getTotalCartAmount();
+
+    const navigate = useNavigate();
+
     return(
         <div className="cart">
+            <div>Twój Koszyk</div>
+
+         <div className="cart">
             {PRODUCTS.map((product)=>{
                 if (cartItems[product.id] !==0) {
                     return <CartItem data={product} />;
                 }
             } )}
+         </div>
 
-
+            {totalAmount > 0 ? (
+                <div className='checkout'>
+                    <p>Suma:{totalAmount}zł</p>
+                    <button onClick={() => navigate('/')}>kontynuuj zakupy</button>
+                    <button
+                        onClick={() => {
+                        checkout();
+                        navigate('/checkout');
+                        }}
+                        >
+                        {' '}
+                        Sprawdzanie{''}
+                    </button>
+                </div>
+                ) : (
+                    <h1>Twój wuzek na zakupy jest pusty</h1>
+                )}
         </div>
     )
 }
+export default Cart;
