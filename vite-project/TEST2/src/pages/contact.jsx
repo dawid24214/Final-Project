842f29f1dfcb7139371ae3236127d41d5540f95a
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-18 19:48:25 +0200

    The last step

--- vite-project/TEST2/src/pages/contact.jsx
+++ vite-project/TEST2/src/pages/contact.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import './_contact.scss';
 
 export const Contact = () => {
-    return <div>contact</div>;
+    return (
+        <section className="contact">
+            <form>
+                <h2>Kontakt</h2>
+                <div className='input=box'>
+                    <label>Imie</label>
+                    <input type='text' className='field' placeholder='Podaj swoje Imie' required/>
+                </div>
+
+                <div className='input=box'>
+                    <label>Adress email</label>
+                    <input type='text' className='field' placeholder='Podaj swoj email' required/>
+                </div>
+
+                <div className='input=box'>
+                    <label>Twoja waidomosc</label>
+                    <input name='' id='' className='field' placeholder='Napisz swoja wiadomość' required/>
+                </div>
+                <button type='submit'>Wyslij wiadomość</button>
+            </form>
+        </section>
+    );
 };
