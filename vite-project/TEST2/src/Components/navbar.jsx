c9808592499dbe16c007ffaa48c870b5970b3d00
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-21 13:11:14 +0200

    Updated the Navbar component using ShopContext
    Added display of the total number of products in the cart icon
    Introduced styling for the counter in navbar.scss
    The change improves the visibility of the cart status for the user.

--- vite-project/TEST2/src/Components/navbar.jsx
+++ vite-project/TEST2/src/Components/navbar.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { ShoppingCart } from "phosphor-react";
+import React, {useContext} from "react";
+import {Link} from "react-router-dom";
+import {ShoppingCart} from "phosphor-react";
 import "./_navbar.scss";
+import {ShopContext} from "../pages/context/Shop-context.jsx";
 
 export const Navbar = () => {
+    const {cartItems} = useContext(ShopContext);
+    const totalItemsInCart = Object.values(cartItems).reduce((acc, itemCount) => acc + itemCount, 0);
+
     return (
         <div className="navbar">
             <div className="links">
                 <Link to="/"> Sklep </Link>
                 <Link to="/contact"> Kontakt </Link>
                 <Link to="/cart">
-                    <ShoppingCart size={32} />
+                    <div className='cart-icon'>
+                        <ShoppingCart size={32}/>
+                        {totalItemsInCart > 0 && (
+                            <span className='cart-count'>{totalItemsInCart}</span>
+                        )}
+                    </div>
                 </Link>
             </div>
         </div>
--- vite-project/TEST2/src/pages/shop/product.jsx
+++ vite-project/TEST2/src/pages/shop/product.jsx
@@ -1,23 +1,39 @@
-import React, { useContext } from "react";
-import { ShopContext } from "../context/Shop-context.jsx";
+import React, {useContext} from "react";
+import {ShopContext} from "../context/Shop-context.jsx";
+import {ShoppingCart} from "phosphor-react"; // Zmiana importu
 
 export const Product = (props) => {
-    const { id, productName, price, productImage } = props.data;
-    const { addToCart, cartItems } = useContext(ShopContext);
+    const {id, productName, price, productImage} = props.data;
+    const {addToCart, cartItems} = useContext(ShopContext);
 
     const cartItemCount = cartItems[id];
 
+    const iconDisplay = () => {
+        if (cartItemCount > 0) {
+            return (
+                <div className="cart-icon">
+                    <ShoppingCart size={24}/> {/* Użycie komponentu ShoppingCart */}
+                    <span className="cart-count">{cartItemCount}</span>
+                </div>
+            );
+        } else {
+            return <ShoppingCart size={24}/>;
+            {/* Użycie komponentu ShoppingCart */
+            }
+        }
+    };
+
     return (
         <div className="product">
-            <img src={productImage} />
+            <img src={productImage} alt={productName}/>
             <div className="description">
                 <p>
                     <b>{productName}</b>
                 </p>
-                <p> {price}zł</p>
+                <p>{price}zł</p>
             </div>
             <button className="addToCartBttn" onClick={() => addToCart(id)}>
-                Dodaj produkt {cartItemCount > 0 && <> ({cartItemCount})</>}
+                Dodaj produkt
             </button>
         </div>
     );
