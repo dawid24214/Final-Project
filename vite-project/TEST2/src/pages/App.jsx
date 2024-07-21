e3e707d1abae12df305a1ed650edbe949de76a8a
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-21 09:22:59 +0200

    Zmiana struktury i definicja komponentu

--- vite-project/TEST2/src/pages/App.jsx
+++ vite-project/TEST2/src/pages/App.jsx
@@ -1,25 +1,39 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "../Components/navbar.jsx";
-import { Shop } from "./shop/shop.jsx";
-import { Contact } from "./contact.jsx";
-import { Cart } from "./Cart/cart.jsx";
-import { ShopContextProvider } from "./context/Shop-context.jsx";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
+import {Navbar} from "../Components/navbar.jsx";
+import {Shop} from "./shop/shop.jsx";
+import {Contact} from "./contact.jsx";
+import {Cart} from "./Cart/cart.jsx";
+import {ShopContextProvider} from "./context/Shop-context.jsx";
+
+const Layout = () => {
+    return (
+        <div className="App">
+            <Navbar/>
+            <Outlet/>
+        </div>
+    )
+}
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Shop/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
 
 function App() {
     return (
-        <div className="App">
-            <ShopContextProvider>
-                <Router>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Shop />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/cart" element={<Cart />} />
-                    </Routes>
-                </Router>
-            </ShopContextProvider>
-        </div>
+        <ShopContextProvider>
+            <Router/>
+        </ShopContextProvider>
     );
 }
 
