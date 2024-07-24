8ee94c794a2d97113321de3f1ae3c8a7798570ec
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-07-24 16:04:49 +0200

    Execution of the order form.

--- vite-project/TEST2/src/pages/OrderNow/Order.jsx
+++ vite-project/TEST2/src/pages/OrderNow/Order.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- const OrderApp = () =>{
-    return<div>Jestem Dawid</div>
-};
+ const OrderApp = () => {
+    return (
+        <section className="contact">
+            <form>
+                <h2>Szczegóły płatności </h2>
+                <div className='input-box'>
+                    <label>Imię i Nazwisko*</label>
+                    <input type='text' className='field' placeholder='Podaj swoje Imie' required/>
+                </div>
 
+                <div className='input-box'>
+                    <label>Kraj*</label>
+                    <input type='text' className='field' placeholder='Podaj swoj email' required/>
+                </div>
+
+                <div className='input-box'>
+                    <label>Adres*</label>
+                    <input name='' id='' className='field' placeholder='Napisz swoja wiadomość' required/>
+                </div>
+
+                <div className='input-box'>
+                    <label>Miasto*</label>
+                    <input name='' id='' className='field' placeholder='' required/>
+                </div>
+
+                <div className='input-box'>
+                    <label>Telefon*</label>
+                    <input name='' id='' className='field' placeholder='' required/>
+                </div>
+
+                <div className='input-box'>
+                    <label>Email*</label>
+                    <input name='' id='' className='field' placeholder='' required/>
+                </div>
+
+                <div className='input-box'>
+                    <label>Uwagi do zamówienia (np. dodatkowe informacje o adresie)</label>
+                    <input name='' id='' className='field' placeholder='' required/>
+                </div>
+                <div className='input-box'>
+                    <label>Chcę fakturę i wyrażam zgodę na jej wysyłkę drogą elektroniczną</label>
+                    <input type='checkbox'/>
+                </div>
+
+
+                <button type='submit'>Zatwierdz</button>
+            </form>
+        </section>
+    );
+ };
 export default OrderApp;
--- vite-project/TEST2/src/pages/contact.jsx
+++ vite-project/TEST2/src/pages/contact.jsx
@@ -6,17 +6,17 @@ export const Contact = () => {
         <section className="contact">
             <form>
                 <h2>Kontakt</h2>
-                <div className='input=box'>
+                <div className='input-box'>
                     <label>Imie</label>
                     <input type='text' className='field' placeholder='Podaj swoje Imie' required/>
                 </div>
 
-                <div className='input=box'>
+                <div className='input-box'>
                     <label>Adress email</label>
                     <input type='text' className='field' placeholder='Podaj swoj email' required/>
                 </div>
 
-                <div className='input=box'>
+                <div className='input-box'>
                     <label>Twoja waidomosc</label>
                     <input name='' id='' className='field' placeholder='Napisz swoja wiadomość' required/>
                 </div>
